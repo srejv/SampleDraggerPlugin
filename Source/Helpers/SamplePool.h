cae45ab52fb94578ed9037f3200d014240ed2542
Oscar Dragén <oscar.dragen@elektron.se>
2017-11-07 13:26:08 +0100

    Warning fixes

--- Source/Helpers/SamplePool.h
+++ Source/Helpers/SamplePool.h
@@ -28,14 +28,13 @@ public:
     ScopedPointer<AudioFormatReader> reader(formatManager.createReaderFor(file));
     if (reader == nullptr) return -1;
 
-    const double duration = reader->lengthInSamples / reader->sampleRate;
     SampleData data;
     data.name = file.getFileNameWithoutExtension();
     data.buffer.setSize(reader->numChannels, static_cast<int>(reader->lengthInSamples));
     reader->read(&data.buffer, 0, static_cast<int>(reader->lengthInSamples), 0, true, true);
 
     samplePool.push_back(data);
-    return samplePool.size() - 1;
+    return static_cast<int>(samplePool.size()) - 1;
   }
 
 private:
